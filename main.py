7bf9929879125f9da179358981ed2d9cf425c379
JuanPlaz <juanplaz3@gmail.com>
2023-04-21 13:36:30 +1000

    Add comments

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI() ##Instancia de FastAPI
 
 @app.get("/") ##Path Operation decorator (Usa metodo get).
-def home():
-    return {"Hello": "World"}
+def home():    ##Path operation function
+    return {"Hello": "World"}   ##Json
