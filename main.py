ae6994d94413a95095b466fc938e7e1edda3569e
JuanPlaz <juanplaz3@gmail.com>
2023-04-22 12:37:47 +1000

    Se definen funciones

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI() ##Instancia de FastAPI
 @app.get("/") ##Path Operation decorator (Usa metodo get).
 def home():    ##Path operation function
     return {"Hello": "World"}   ##Json
+
+@app.get("/ciudades")
+def ciudades():
+    return {"Colombia":"Bogota"}
+
+@app.get("/items/{num_item}")
+async def items(num_item):
+    return {"num_item":num_item}
