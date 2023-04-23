14b8907b2d6682411fcbd198aad34fa305224c2d
JuanPlaz <juanplaz3@gmail.com>
2023-04-23 18:36:10 +1000

    Constructor y request Body

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI() ##Instancia de FastAPI
+
+#Models
+class Person(BaseModel):    ##Creación de constructor con Pydantic
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None    #Atributos opcionales de la clase Person
+    is_married: Optional[bool] = None
+
 
 @app.get("/") ##Path Operation decorator (Usa metodo get).
 def home():    ##Path operation function
@@ -8,9 +26,11 @@ def home():    ##Path operation function
 
 #Request and Response Body 
 @app.post("/person/new")
-def create_person():
-    pass 
+def create_person(person: Person = Body(...)):  #Parámetro person de clase Person. Request Body obligatorio.
+    return person
 
+
+"""
 @app.get("/ciudades")
 def ciudades():
     return {"Colombia":"Bogota"}
@@ -18,3 +38,4 @@ def ciudades():
 @app.get("/items/{num_item}")
 async def items(num_item):
     return {"num_item":num_item}
+    """
